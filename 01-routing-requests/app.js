849a13211aa2452e7fc91e569996c8d2da35d929
Stanislau Hekht <hekhtstanislau@gmail.com>
2023-06-23 01:53:58 -0400

    Parse request bodies

--- 01-routing-requests/app.js
+++ 01-routing-requests/app.js
@@ -6,33 +6,17 @@ const server = http.createServer((req, res) => {
   const method = req.method;
   if (url === '/') {
     res.write('<html>');
-    res.write('<head><title>Enter message</title></head>');
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-    );
+    res.write('<head><title>Enter Message</title><head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
     res.write('</html>');
     return res.end();
   }
-
   if (url === '/message' && method === 'POST') {
-    const body = [];
-
-    req.on('data', (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      let message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
-    });
-
+    fs.writeFileSync('message.txt', 'DUMMY');
     res.statusCode = 302;
     res.setHeader('Location', '/');
     return res.end();
   }
-
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title><head>');
@@ -41,4 +25,4 @@ const server = http.createServer((req, res) => {
   res.end();
 });
 
-server.listen(5000);
+server.listen(3000);
