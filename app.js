80422c7f809540d18fff2635cf469a15718f2278
Stanislau Hekht <hekhtstanislau@gmail.com>
2023-06-26 01:06:36 -0400

    Complete express module

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-  console.log('This always runs');
-  next();
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware');
-  res.send('<h1>Add product page</h1/>');
-});
+// Register middleware
+app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1/>');
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 const host = 'localhost';
