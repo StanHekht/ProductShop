dd29f5692abed78e62cb62a622e5a419f0d0be08
Stanislau Hekht <hekhtstanislau@gmail.com>
2023-07-06 23:20:51 -0400

    Have basic routes render pug templates instead of plain html

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 // Register middleware
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404');
 });
 
 const host = 'localhost';
